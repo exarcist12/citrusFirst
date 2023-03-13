fa09c4b99eff519d5da7bcf7b8b883570e419712
exarcist12 <exarcist12@gmail.com>
2023-03-13 09:48:33 +0400

    dcsc

---
+++ src/test/java/lesson/tests/EchoTest.java
@@ -0,0 +1,90 @@
+package lesson.tests;
+
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.context.TestContext;
+import com.consol.citrus.testng.TestNGCitrusSupport;
+import lesson.pojo.Data;
+import lesson.pojo.Support;
+import lesson.pojo.User;
+import org.testng.annotations.Test;
+
+import static com.consol.citrus.actions.EchoAction.Builder.echo;
+
+public class EchoTest extends TestNGCitrusSupport {
+
+	private TestContext context;
+
+	@Test(description = "Получение информации о пользователе", enabled=true)
+	@CitrusTest
+	public void getTestActions() {
+		this.context = citrus.getCitrusContext().createTestContext();
+
+		context.setVariable("value", "superValue");
+		$(echo("Property \"value\" = " + context.getVariable("value")));
+
+//		$(echo("We have userId = " + context.getVariable("userId")));
+		$(echo("Property \"userId\" = " + "${userId}"));
+
+		variable("now", "citrus:currentDate()");
+		$(echo("Today is: ${now}"));
+
+//		$(http()
+//				.client("restClientReqres")
+//				.send()
+//				//.get("users/${userId}")
+//				.get("users/" + context.getVariable("userId"))
+//		);
+//
+//		$(http()
+//						.client("restClientReqres")
+//						.receive()
+//						.response(HttpStatus.OK)
+//						.message()
+//						.type("application/json")
+//						.body(new ObjectMappingPayloadBuilder(getJsonData(), "objectMapper"))
+//		);
+	}
+
+
+//------------------------------------------------------------------------------
+						//json string
+//                .body("{\n" +
+//                "    \"data\": {\n" +
+//                "        \"id\": 2,\n" +
+//                "        \"email\": \"janet.weaver@reqres.in\",\n" +
+//                "        \"first_name\": \"Janet\",\n" +
+//                "        \"last_name\": \"Weaver\",\n" +
+//                "        \"avatar\": \"https://reqres.in/img/faces/2-image.jpg\"\n" +
+//                "    },\n" +
+//                "    \"support\": {\n" +
+//                "        \"url\": \"https://reqres.in/#support-heading\",\n" +
+//                "        \"text\": \"To keep ReqRes free, contributions towards server costs are appreciated!\"\n" +
+//                "    }\n" +
+//                "}")
+
+						//json POJO
+
+				//json string
+//                .body(new ClassPathResource("json/user2.json"))
+//-----------------------------------------------------------------------------
+
+	public User getJsonData() {
+		User user = new User();
+
+		Data data = new Data();
+		data.setId(Integer.valueOf(context.getVariable("userId")));
+		data.setEmail("janet.weaver@reqres.in");
+		data.setFirstName("Janet");
+		data.setLastName("Weaver");
+		data.setAvatar("https://reqres.in/img/faces/2-image.jpg");
+		user.setData(data);
+
+		Support support = new Support();
+		support.setUrl("https://reqres.in/#support-heading");
+		support.setText("To keep ReqRes free, contributions towards server costs are appreciated!");
+
+		user.setSupport(support);
+
+		return user;
+	}
+}
